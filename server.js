389e2b7c40e550029e526024b0d69bb8404604b8
Mari Kas <mkas@gmail.com~>
2022-04-25 14:31:25 +0300

    add changes

---
+++ server.js
@@ -0,0 +1,19 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html');
+
+});
+
+app.get('/about', (request, response)=>{
+    response.send('Hello my name is Greete!');
+
+});
+
+app.listen(3000, ()=>{
+    console.log('Server is running on Port 3000.')
+});
